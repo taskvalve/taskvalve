dc78c41d58822d5d08fab3229cc300df1dbfd129
Richard McDaniel <richard.lee.mcdaniel@gmail.com>
2023-10-17 02:24:17 +0000

    Fix MySQL

--- model/mysql.ts
+++ model/mysql.ts
@@ -17,7 +17,7 @@ export class MySQL implements IModel {
         public storedWorkflowClass: string = 'Workflow\\Models\\StoredWorkflow',
     ) {}
 
-    async insert(workflowId: number, method: string, args: any[]): Promise<void> {
+    async create(workflowClass: string, args: any[]): Promise<number> {
         let template = "O:46:\"Laravel\\SerializableClosure\\Serializers\\Native\":5:{s:3:\"use\";a:1:{s:4:\"data\";a:1:{i:0;b:1;}}s:8:\"function\";s:21:\"static fn () => $data\";s:5:\"scope\";s:22:\"Workflow\\Serializers\\Y\";s:4:\"this\";N;s:4:\"self\";s:32:\"00000000000000000000000000000000\";}"
 
         const client = await new Client().connect({
@@ -29,19 +29,50 @@ export class MySQL implements IModel {
         });
 
         const serialized = serialize(args)
-        const serializedArgs = template.replace(/s:4:"data";a:\d+:\{[^}]+\}/, `s:4:"data";${serialized}`);
+        const serializedArgs = template.replace(/s:4:"data";a:\d+:\{[^}]+\}/, `s:4:"data";${serialized}`)
         const hash = await this.crypto.hmac(serializedArgs)
 
         const encoder = new TextEncoder()
         const argsLength = encoder.encode(serializedArgs).length
         const hashLength = encoder.encode(hash).length
 
-        let serializedClosure = `O:47:"Laravel\\SerializableClosure\\SerializableClosure":1:{s:12:"serializable";O:46:"Laravel\\SerializableClosure\\Serializers\\Signed":2:{s:12:"serializable";s:${argsLength}:"${serializedArgs}";s:4:"hash";s:${hashLength}:"${hash}";}}`;
+        let serializedClosure = `O:47:"Laravel\\SerializableClosure\\SerializableClosure":1:{s:12:"serializable";O:46:"Laravel\\SerializableClosure\\Serializers\\Signed":2:{s:12:"serializable";s:${argsLength}:"${serializedArgs}";s:4:"hash";s:${hashLength}:"${hash}";}}`
+
+        const results = await client.execute(
+            `INSERT INTO workflows(class, arguments, status) VALUES(?, ?, ?)`,
+            [workflowClass, serializedClosure, 'created']
+        )
+
+        await client.close()
+
+        return results.lastInsertId
+    }
+
+    async insert(workflowId: number, method: string, args: any[]): Promise<void> {
+        let template = "O:46:\"Laravel\\SerializableClosure\\Serializers\\Native\":5:{s:3:\"use\";a:1:{s:4:\"data\";a:1:{i:0;b:1;}}s:8:\"function\";s:21:\"static fn () => $data\";s:5:\"scope\";s:22:\"Workflow\\Serializers\\Y\";s:4:\"this\";N;s:4:\"self\";s:32:\"00000000000000000000000000000000\";}"
+
+        const client = await new Client().connect({
+            hostname: this.hostname,
+            port: parseInt(this.port),
+            username: this.username,
+            db: this.db,
+            password: this.password
+        })
+
+        const serialized = serialize(args)
+        const serializedArgs = template.replace(/s:4:"data";a:\d+:\{[^}]+\}/, `s:4:"data";${serialized}`)
+        const hash = await this.crypto.hmac(serializedArgs)
+
+        const encoder = new TextEncoder()
+        const argsLength = encoder.encode(serializedArgs).length
+        const hashLength = encoder.encode(hash).length
+
+        let serializedClosure = `O:47:"Laravel\\SerializableClosure\\SerializableClosure":1:{s:12:"serializable";O:46:"Laravel\\SerializableClosure\\Serializers\\Signed":2:{s:12:"serializable";s:${argsLength}:"${serializedArgs}";s:4:"hash";s:${hashLength}:"${hash}";}}`
 
         await client.execute(
             `INSERT INTO workflow_signals(stored_workflow_id, method, arguments) VALUES(?, ?, ?)`,
             [workflowId, method, serializedClosure]
-        );
+        )
 
         await client.close()
     }
--- model/pgsql.ts
+++ model/pgsql.ts
@@ -1,9 +1,9 @@
-import { IModel } from './interface.ts';
-import { ICrypto } from '../crypto.ts';
+import { IModel } from './interface.ts'
+import { ICrypto } from '../crypto.ts'
 
-import { Client } from "https://deno.land/x/postgres/mod.ts";
+import { Client } from "https://deno.land/x/postgres/mod.ts"
 
-import { serialize } from '../serialize.ts';
+import { serialize } from '../serialize.ts'
 
 export class PostgreSQL implements IModel {
     constructor(
@@ -18,7 +18,7 @@ export class PostgreSQL implements IModel {
     ) {}
 
     async create(workflowClass: string, args: any[]): Promise<number> {
-        let template = "O:46:\"Laravel\\SerializableClosure\\Serializers\\Native\":5:{s:3:\"use\";a:1:{s:4:\"data\";a:1:{i:0;b:1;}}s:8:\"function\";s:21:\"static fn () => $data\";s:5:\"scope\";s:22:\"Workflow\\Serializers\\Y\";s:4:\"this\";N;s:4:\"self\";s:32:\"00000000000000000000000000000000\";}";
+        let template = "O:46:\"Laravel\\SerializableClosure\\Serializers\\Native\":5:{s:3:\"use\";a:1:{s:4:\"data\";a:1:{i:0;b:1;}}s:8:\"function\";s:21:\"static fn () => $data\";s:5:\"scope\";s:22:\"Workflow\\Serializers\\Y\";s:4:\"this\";N;s:4:\"self\";s:32:\"00000000000000000000000000000000\";}"
 
         const client = new Client({
             hostname: this.hostname,
@@ -26,24 +26,24 @@ export class PostgreSQL implements IModel {
             user: this.user,
             database: this.db,
             password: this.password,
-        });
+        })
 
-        await client.connect();
+        await client.connect()
 
-        const serialized = serialize(args);
-        const serializedArgs = template.replace(/s:4:"data";a:\d+:\{[^}]+\}/, `s:4:"data";${serialized}`);
-        const hash = await this.crypto.hmac(serializedArgs);
+        const serialized = serialize(args)
+        const serializedArgs = template.replace(/s:4:"data";a:\d+:\{[^}]+\}/, `s:4:"data";${serialized}`)
+        const hash = await this.crypto.hmac(serializedArgs)
 
-        const encoder = new TextEncoder();
-        const argsLength = encoder.encode(serializedArgs).length;
-        const hashLength = encoder.encode(hash).length;
+        const encoder = new TextEncoder()
+        const argsLength = encoder.encode(serializedArgs).length
+        const hashLength = encoder.encode(hash).length
 
-        let serializedClosure = `O:47:"Laravel\\SerializableClosure\\SerializableClosure":1:{s:12:"serializable";O:46:"Laravel\\SerializableClosure\\Serializers\\Signed":2:{s:12:"serializable";s:${argsLength}:"${serializedArgs}";s:4:"hash";s:${hashLength}:"${hash}";}}`;
+        let serializedClosure = `O:47:"Laravel\\SerializableClosure\\SerializableClosure":1:{s:12:"serializable";O:46:"Laravel\\SerializableClosure\\Serializers\\Signed":2:{s:12:"serializable";s:${argsLength}:"${serializedArgs}";s:4:"hash";s:${hashLength}:"${hash}";}}`
 
         const results = await client.queryObject(
             `INSERT INTO workflows(class, arguments, status) VALUES($1, $2, $3) RETURNING id`,
             [workflowClass, serializedClosure, 'created']
-        );
+        )
 
         await client.end()
 
@@ -59,24 +59,24 @@ export class PostgreSQL implements IModel {
             user: this.user,
             database: this.db,
             password: this.password,
-        });
+        })
 
-        await client.connect();
+        await client.connect()
 
-        const serialized = serialize(args);
-        const serializedArgs = template.replace(/s:4:"data";a:\d+:\{[^}]+\}/, `s:4:"data";${serialized}`);
-        const hash = await this.crypto.hmac(serializedArgs);
+        const serialized = serialize(args)
+        const serializedArgs = template.replace(/s:4:"data";a:\d+:\{[^}]+\}/, `s:4:"data";${serialized}`)
+        const hash = await this.crypto.hmac(serializedArgs)
 
-        const encoder = new TextEncoder();
-        const argsLength = encoder.encode(serializedArgs).length;
-        const hashLength = encoder.encode(hash).length;
+        const encoder = new TextEncoder()
+        const argsLength = encoder.encode(serializedArgs).length
+        const hashLength = encoder.encode(hash).length
 
-        let serializedClosure = `O:47:"Laravel\\SerializableClosure\\SerializableClosure":1:{s:12:"serializable";O:46:"Laravel\\SerializableClosure\\Serializers\\Signed":2:{s:12:"serializable";s:${argsLength}:"${serializedArgs}";s:4:"hash";s:${hashLength}:"${hash}";}}`;
+        let serializedClosure = `O:47:"Laravel\\SerializableClosure\\SerializableClosure":1:{s:12:"serializable";O:46:"Laravel\\SerializableClosure\\Serializers\\Signed":2:{s:12:"serializable";s:${argsLength}:"${serializedArgs}";s:4:"hash";s:${hashLength}:"${hash}";}}`
 
-        await client.queryArray(
+        await client.queryObject(
             `INSERT INTO workflow_signals(stored_workflow_id, method, arguments) VALUES($1, $2, $3)`,
             [workflowId, method, serializedClosure]
-        );
+        )
 
         await client.end()
     }
